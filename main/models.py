df43a3f446594421d4d86f5ca35063b0d6060b66
gkipping2023 <gkipping01@gmail.com>
2024-06-13 17:19:33 -0500

    updated model.py for security

--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-#SUPERUSER: george/ EMAIL: george@george.com/ PASSWORD: panama2024
 # Create your models here.
 class User(AbstractUser):
     nombre = models.CharField(max_length=250)
